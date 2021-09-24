00d62986f8403111944d7c47500cab63b6c36284
carnage87 <walter.carnage@gmail.com>
2021-09-23 21:38:46 -0300

    Finalizado o código e testado com sucesso

--- Composition2/Entities/Post.cs
+++ Composition2/Entities/Post.cs
@@ -35,5 +35,21 @@ namespace Composition2.Entities
         {
             Comments.Remove(comment);
         }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments");
+            foreach (Comment C in Comments)
+            {
+                sb.AppendLine(C.Text);
+            }
+            return sb.ToString();
+        }
     }
 }
